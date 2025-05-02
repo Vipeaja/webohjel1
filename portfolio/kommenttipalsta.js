297df5da850b0435895fe2b9c09b7891210c9541
Vipeaja <aatos.koskelainen@gmail.com>
2025-05-02 14:29:26 +0300

    kommentittoimii

--- portfolio/kommenttipalsta.js
+++ portfolio/kommenttipalsta.js
@@ -1,3 +1,30 @@
 function hello() {
     alert("Hello world!");
 }
+
+function sendet() {
+    birthdayField = document.getElementById("synty").value
+    emailField = document.getElementById("email").value
+    typeField = document.getElementById("viesti").value
+    usageField = document.getElementById("cookies").value
+    bodyField = document.getElementById("texti").value
+
+    console.log(birthdayField)
+    console.log(emailField)
+    console.log(typeField)
+    console.log(usageField)
+    console.log(bodyField)
+
+}
+
+
+
+function closedial() {
+    dialog = document.getElementById("dialo")
+    dialog.close()
+}
+
+function opendial() {
+    dialog = document.getElementById("dialo");
+    dialog.showModal();
+}

527bcf319777c48c07771969a6657d9519dcbd4c
Vipeaja <aatos.koskelainen@gmail.com>
2025-05-15 11:34:45 +0300

    päivämäärä

--- portfolio/muutujia_ja_tietotyyppejä.js
+++ portfolio/muutujia_ja_tietotyyppejä.js
@@ -47,3 +47,22 @@ function laskin_jako(){
 
 
 
+function aika(){
+    var päivä = ["Sunnuntai","Maanantai","Tiistai","Keskiviikko","Torstai","Perjantai","Lauantai"]
+    var kuukausit = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+
+    var päiväys = new Date()
+    
+    var paivamaar = document.getElementById("paivamaara")
+    var aika = document.getElementById("aika")
+    var paivaJaAika = document.getElementById("paivaJaAika")
+    var viikonpaiva = document.getElementById("viikonpaiva")
+    var kuukausi = document.getElementById("kuukausi")
+    aika.innerHTML = päiväys.toLocaleTimeString()
+    paivamaar.innerHTML = päiväys.toLocaleDateString()
+    paivaJaAika.innerHTML = päiväys.toLocaleString()
+    viikonpaiva.innerHTML = päivä[päiväys.getDay()]
+    kuukausi.innerHTML = kuukausit[päiväys.getMonth()]
+    
+
+}
